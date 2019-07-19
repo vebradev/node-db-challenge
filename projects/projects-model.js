caf679337c9c5448f0d40fe575a4f90fb4c35a37
vebradev <justinas@vebra.dev>
2019-07-19 11:50:34 +0300

    Add functionality to pull project data with actions

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -2,5 +2,21 @@ const knex = require('knex');
 const db = knex(require('../knexfile').development);
 
 module.exports = {
+  getProject,
+  getActions
+}
 
+function getProject(id) {
+  return db
+    .select(knex.raw("id as id, name as name, desc as description, isCompleted as completed"))
+    .from("projects")
+    .where("projects.id", id)
+}
+
+function getActions(id) {
+  return db
+    .select(knex.raw("actions.id as id, actions.desc as description, notes as notes, actions.isCompleted as completed"))
+    .from("projects")
+    .leftJoin("actions", "projects.id", "actions.projectId")
+    .where("projects.id", id)
 }
--- projects/projects-router.js
+++ projects/projects-router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Project = require("./projects-model");
+const Projects = require("./projects-model");
 
 const router = express.Router();
 
@@ -9,6 +9,24 @@ router.get("/", async (req, res) => {
   } catch (error) {
     res.status(500).json({
       error: "Failed to load recipes."
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const project = await Projects.getProject(req.params.id);
+    const actions = await Projects.getActions(req.params.id);
+    res.json({
+      "id": project[0].id,
+      "name": project[0].name,
+      "description": project[0].description,
+      "completed": project[0].completed,
+      "actions" : actions
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Failed to load project and it's actions."
     });
   }
 });
